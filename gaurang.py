6f98ea9f6f47d837ac5517fd0198e79c087ec4a5
Gaurang_Sharma <gaurangsh08n@gmail.com>
2023-10-02 17:54:05 +0530

    Ho gaya connect

--- gaurang.py
+++ gaurang.py
@@ -1,1 +1,18 @@
-print("Hello great grand mother fuckers")
+import socket
+
+host = '192.168.0.101'
+port = 3006
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen(1)
+
+print(f"Server listening on {host}:{port}")
+
+conn, addr = server_socket.accept()
+print(f"Connected by {addr}")
+
+data = conn.recv(1024)
+print(f"Received data: {data.decode()}")
+
+conn.close()
